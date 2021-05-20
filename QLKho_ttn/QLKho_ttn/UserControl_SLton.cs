10a8bc896d3d988cf34f0b61b76e18462c0dbdbd
mrkoc224 <nguyenducduy224@gmail.com>
2021-05-20 20:46:06 +0700

    bind data to gridview

--- QLKho_ttn/QLKho_ttn/UserControl_SLton.cs
+++ QLKho_ttn/QLKho_ttn/UserControl_SLton.cs
@@ -19,5 +19,27 @@ namespace QLKho_ttn
         }
         SqlConnection sqlc = new SqlConnection(SQL_Connect.ConnectionString);
 
+
+        // bind data to gridview  Duy
+        private void UserControl_SLton_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlc.Open();
+                string Myquery = @"select dbo.Object_.ID as ID,dbo.Object_.DisplayName as N'Tên sản phẩm',dbo.ObjectType.DisplayName as N'Phân loại',dbo.Suplier.DisplayName as N'Nhà cung cấp',dbo.Object_.Quantity as N'Số lượng tồn',dbo.Object_.InputPrice as N'Giá nhập',dbo.Object_.OutputPrice as N'Giá xuất' from dbo.Suplier join dbo.Object_ on Suplier.ID = Object_.IDSuplier join dbo.ObjectType on Object_.IDType = ObjectType.ID";
+                SqlDataAdapter sqla = new SqlDataAdapter(Myquery, sqlc);
+                SqlCommandBuilder builder = new SqlCommandBuilder(sqla);
+                var ds = new DataSet();
+                sqla.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                sqlc.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+
     }
 }
