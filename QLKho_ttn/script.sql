e7ebe27b673fa27a7064d899b8489fadd73905c0
QuangHuyNguyen <83232436+ngquanghuy2000@users.noreply.github.com>
2021-05-20 17:27:27 +0700

    Update script.sql

--- QLKho_ttn/script.sql
+++ QLKho_ttn/script.sql
@@ -1,4 +1,4 @@
-﻿create database QLK_ttn
+create database QLK_ttn
 go
 use QLK_ttn
 go
@@ -68,3 +68,27 @@ create table OutputInfo(
 )
 go
 insert into User_(DisplayName,UserName,Password_,AdminRole) values(N'Người quản trị','admin','admin',1)
+insert into ObjectType(DisplayName) values (N'Loại 1')
+insert into ObjectType(DisplayName) values (N'Loại 2')
+insert into ObjectType(DisplayName) values (N'Loại 3')
+insert into ObjectType(DisplayName) values (N'Loại 4')
+insert into ObjectType(DisplayName) values (N'Loại 5')
+insert into Suplier(DisplayName,Phone,Mail,Address_) values(N'Nhà cung cấp 1','9999-999-999','suplier1@email.com',N'Hà Nội')
+insert into Suplier(DisplayName,Phone,Mail,Address_) values(N'Nhà cung cấp 2','8888-888-888','suplier2@email.com',N'Hà Nội')
+insert into Suplier(DisplayName,Phone,Mail,Address_) values(N'Nhà cung cấp 3','7777-777-777','suplier3@email.com',N'TP. Hồ Chí Minh')
+insert into Suplier(DisplayName,Phone,Mail,Address_) values(N'Nhà cung cấp 4','6666-666-666','suplier4@email.com',N'TP. Hồ Chí Minh')
+insert into Suplier(DisplayName,Phone,Mail,Address_) values(N'Nhà cung cấp 5','5555-555-555','suplier5@email.com',N'Hà Nội')
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ001',N'Sản phẩm 1',1,1,1000,'true',100000,105000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ002',N'Sản phẩm 2',1,2,1000,'true',200000,210000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ003',N'Sản phẩm 3',3,1,1000,'true',150000,160000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ004',N'Sản phẩm 4',4,3,1000,'true',350000,355000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ005',N'Sản phẩm 5',5,1,1000,'true',240000,255000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ006',N'Sản phẩm 6',2,4,1000,'true',160000,175000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ007',N'Sản phẩm 7',4,3,1000,'true',250000,255000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ008',N'Sản phẩm 8',5,5,1000,'true',300000,305000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ009',N'Sản phẩm 9',2,1,1000,'false',400000,405000)
+insert into Object_(ID,DisplayName,IDType,IDSuplier,Quantity,Available,InputPrice,OutputPrice) values('OBJ010',N'Sản phẩm 10',3,3,1000,'true',330000,345000)
+insert into Customer(DisplayName,Phone,Mail,Address_) values(N'Khách hàng 1','1111-111-111','customer1@email.com',N'Hà Nội')
+insert into Customer(DisplayName,Phone,Mail,Address_) values(N'Khách hàng 2','2222-222-222','customer2@email.com',N'Hà Nội')
+insert into Customer(DisplayName,Phone,Mail,Address_) values(N'Khách hàng 3','3333-333-333','customer3@email.com',N'Hà Nội')
+insert into Customer(DisplayName,Phone,Mail,Address_) values(N'Khách hàng 4','4444-444-444','customer1@email.com',N'Hà Nội')
