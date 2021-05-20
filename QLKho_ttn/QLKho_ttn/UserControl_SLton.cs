281e28a8ba4a089fe6bf356a941951475a314ace
QuangHuyNguyen <83232436+ngquanghuy2000@users.noreply.github.com>
2021-05-20 20:44:25 +0700

    small fix by huy

--- QLKho_ttn/QLKho_ttn/UserControl_SLton.cs
+++ QLKho_ttn/QLKho_ttn/UserControl_SLton.cs
@@ -18,23 +18,6 @@ namespace QLKho_ttn
             InitializeComponent();
         }
         SqlConnection sqlc = new SqlConnection(SQL_Connect.ConnectionString);
-        private void UserControl_SLton_Load(object sender, EventArgs e)
-        {
-            try
-            {
-                sqlc.Open();
-                string Myquery = @"select dbo.Object_.ID as ID,dbo.Object_.DisplayName as N'Tên sản phẩm',dbo.ObjectType.DisplayName as N'Phân loại',dbo.Suplier.DisplayName as N'Nhà cung cấp',dbo.Object_.Quantity as N'Số lượng tồn',dbo.Object_.InputPrice as N'Giá nhập',dbo.Object_.OutputPrice as N'Giá xuất' from dbo.Suplier join dbo.Object_ on Suplier.ID = Object_.IDSuplier join dbo.ObjectType on Object_.IDType = ObjectType.ID";
-                SqlDataAdapter sqla = new SqlDataAdapter(Myquery, sqlc);
-                SqlCommandBuilder builder = new SqlCommandBuilder(sqla);
-                var ds = new DataSet();
-                sqla.Fill(ds);
-                dataGridView1.DataSource = ds.Tables[0];
-                sqlc.Close();
-            }
-            catch
-            {
 
-            }
-        }
     }
 }
